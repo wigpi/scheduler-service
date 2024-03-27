fc0efa54f5bf89e57abe16a1b1e793f1a5327dad
Julien Flusin <julienflusin3@gmail.com>
2024-03-28 00:50:41 +0100

    remove logging sql requests from sequelize

--- sequelize.js
+++ sequelize.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT,
+    logging: false,
 });
 
 module.exports = sequelize;
