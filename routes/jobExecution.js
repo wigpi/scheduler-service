5151e29ea6daed44afe95582150a8354f4fe4759
Julien Flusin <julienflusin3@gmail.com>
2024-03-28 00:55:51 +0100

    remove response_body in job execution records if execution was successful

--- routes/jobExecution.js
+++ routes/jobExecution.js
@@ -1,73 +1,42 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../models');
-const JobExecution = db.JobExecution;
-
-// Create a new Job Execution
-router.post('/', async (req, res) => {
-  try {
-    const jobExecution = await JobExecution.create(req.body);
-    res.status(201).send(jobExecution);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const JobExecution = db.JobExecution
 
 // Retrieve all Job Executions
 router.get('/', async (req, res) => {
-  try {
-    const jobExecutions = await JobExecution.findAll();
-    res.status(200).send(jobExecutions);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+	try {
+		const jobExecutions = await JobExecution.findAll()
+		res.status(200).send(jobExecutions)
+	} catch (error) {
+		res.status(400).send(error)
+	}
+})
 
 // Retrieve a single Job Execution by Id
 router.get('/:id', async (req, res) => {
-  try {
-    const jobExecution = await JobExecution.findByPk(req.params.id);
-    if (jobExecution) {
-      res.status(200).send(jobExecution);
-    } else {
-      res.status(404).send({ message: 'Job Execution not found!' });
-    }
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+	try {
+		const jobExecution = await JobExecution.findByPk(req.params.id)
+		if (jobExecution) {
+			res.status(200).send(jobExecution)
+		} else {
+			res.status(404).send({ message: 'Job Execution not found!' })
+		}
+	} catch (error) {
+		res.status(400).send(error)
+	}
+})
 
-// Update a Job Execution by Id
-router.put('/:id', async (req, res) => {
-  try {
-    const [updated] = await JobExecution.update(req.body, {
-      where: { execution_id: req.params.id }
-    });
-    if (updated) {
-      const updatedJobExecution = await JobExecution.findByPk(req.params.id);
-      res.status(200).send(updatedJobExecution);
-    } else {
-      res.status(404).send({ message: 'Job Execution not found!' });
-    }
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+// retrieve all Job Executions for a specific Job
+router.get('/job/:jobId', async (req, res) => {
+	try {
+		const jobExecutions = await JobExecution.findAll({
+			where: { job_id: req.params.jobId }
+		})
+		res.status(200).send(jobExecutions)
+	} catch (error) {
+		res.status(400).send(error)
+	}
+})
 
-// Delete a Job Execution by Id
-router.delete('/:id', async (req, res) => {
-  try {
-    const deleted = await JobExecution.destroy({
-      where: { execution_id: req.params.id }
-    });
-    if (deleted) {
-      res.status(204).send();
-    } else {
-      res.status(404).send({ message: 'Job Execution not found!' });
-    }
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-module.exports = router;
+module.exports = router
--- services/JobExecutor.js
+++ services/JobExecutor.js
@@ -1,45 +1,46 @@
-const axios = require('axios');
-const db = require('../models'); 
-const JobExecution = db.JobExecution; // Adjust the model name as necessary
-
+const axios = require('axios')
+const db = require('../models')
+const JobExecution = db.JobExecution // Adjust the model name as necessary
 
 class JobExecutor {
-  static async executeJob(job) {
-    try {
-      // Prepare the HTTP request based on the job details
-      const config = {
-        method: job.http_method,
-        url: job.url,
-        headers: job.headers ? JSON.parse(job.headers) : {},
-        data: job.payload ? JSON.parse(job.payload) : {}
-      };
+	static async executeJob (job) {
+		try {
+			// Prepare the HTTP request based on the job details
+			const config = {
+				method: job.http_method,
+				url: job.url,
+				headers: job.headers ? JSON.parse(job.headers) : {},
+				data: job.payload ? JSON.parse(job.payload) : {}
+			}
 
-      // Execute the HTTP request
-      const response = await axios(config);
+			// Execute the HTTP request
+			const response = await axios(config)
 
-      // Log the successful execution
-      await JobExecution.create({
-        job_id: job.job_id,
-        success: response.status >= 200 && response.status < 300,
-        response_status: response.status,
-        response_body: JSON.stringify(response.data),
-        error_message: null,
-      });
+			// Log the successful execution
+			await JobExecution.create({
+				job_id: job.job_id,
+				success: response.status >= 200 && response.status < 300,
+				response_status: response.status,
+				response_body: null,
+				error_message: null
+			})
 
-      console.log('Job executed successfully:', job.name);
-    } catch (error) {
-      console.error('Error executing job:', job.name, error.message);
+			console.log('Job executed successfully:', job.name)
+		} catch (error) {
+			console.error('Error executing job:', job.name, error.message)
 
-      // Log the failed execution
-      await JobExecution.create({
-        job_id: job.job_id,
-        success: false,
-        response_status: error.response ? error.response.status : null,
-        response_body: error.response ? JSON.stringify(error.response.data) : null,
-        error_message: error.message,
-      });
-    }
-  }
+			// Log the failed execution
+			await JobExecution.create({
+				job_id: job.job_id,
+				success: false,
+				response_status: error.response ? error.response.status : null,
+				response_body: error.response
+				? JSON.stringify(error.response.data)
+				: null,
+				error_message: error.message
+			})
+		}
+	}
 }
 
-module.exports = JobExecutor;
+module.exports = JobExecutor
