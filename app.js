6a413c4073c6670281f223c585a77d2f8b1f8e75
Julien Flusin <julienflusin3@gmail.com>
2024-10-20 22:29:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,30 +7,17 @@ const swaggerDocument = require('./swagger.json');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const syncDatabase = async () => {
-    try {
-        // Sync models in the correct order
-        await db.Job.sync();            // Sync the Job model first
-        await db.JobExecution.sync();    // Then sync JobExecution (which depends on Job)
-        console.log('Database synchronized successfully.');
-    } catch (error) {
-        console.error('Error during database synchronization:', error);
-        throw error;  // Make sure to stop if there's a sync error
-    }
-};
+app.use(express.json());
 
-syncDatabase().then(() => {
-    app.use(express.json());
+const jobRoutes = require('./routes/job');
+const jobExecutionRoutes = require('./routes/jobExecution');
 
-    const jobRoutes = require('./routes/job');
-    const jobExecutionRoutes = require('./routes/jobExecution');
-    app.use('/jobs', jobRoutes);
-    app.use('/jobs-executions', jobExecutionRoutes);
-    app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/jobs', jobRoutes);
+app.use('/jobs-executions', jobExecutionRoutes);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-    app.listen(PORT, async () => {
-        console.log(`Server running on port ${PORT}`);
-        await scheduler.loadAndScheduleJobs();
-        scheduler.setupCleanupTask(); // Initialize the cleanup task
-    });
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+    await scheduler.loadAndScheduleJobs();
+    scheduler.setupCleanupTask(); // Initialize the cleanup task
 });
